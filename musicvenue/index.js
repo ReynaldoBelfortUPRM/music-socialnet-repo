e1da75b5b5d173ff41fe661753ac4a109ad05b40
ReynaldoBelfort <reynaldo.belfort@upr.edu>
2015-10-31 23:11:31 -0400

    URL Update
    URL has been adapted to the new MVC Folder structure.

--- musicvenue/index.js
+++ musicvenue/index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/www'));
 //This code runs when the user enters only the www.musicvenue.me
 //It re-diirects that  link to the welcome page.
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/www/welcome_page.html');
+	res.sendFile(__dirname + '/www/views/welcome_page.html');
 	//res.sendFile(path.join(__dirname, '../www', 'welcome_page.html'));
 });
 
